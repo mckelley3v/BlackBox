c0cd31b8315299caa7d45095694f0be28b15663c
Mike Kelley <mckelley3v@gmail.com>
2016-01-07 11:43:46 -0600

    m1_any_ptr: fixed is_complete type traits for VS2015

--- libs/m1/any_ptr/include/m1/any_ptr.hpp
+++ libs/m1/any_ptr/include/m1/any_ptr.hpp
@@ -7,21 +7,17 @@
 
 namespace m1
 {
-    // =================================================================================================================
-
-    typedef void (*type_id_t)();
-
-    // =================================================================================================================
+    // ================================================================================================================
 
     class any_ptr;
     class const_any_ptr;
 
-    // -----------------------------------------------------------------------------------------------------------------
+    // ----------------------------------------------------------------------------------------------------------------
 
     void swap(any_ptr &lhs, any_ptr &rhs);
     void swap(const_any_ptr &lhs, const_any_ptr &rhs);
 
-    // -----------------------------------------------------------------------------------------------------------------
+    // ----------------------------------------------------------------------------------------------------------------
 
     bool operator == (std::nullptr_t, any_ptr const &rhs);
     bool operator == (std::nullptr_t, const_any_ptr const &rhs);
@@ -32,7 +28,7 @@ namespace m1
     bool operator == (const_any_ptr const &lhs, any_ptr const &rhs);
     bool operator == (const_any_ptr const &lhs, const_any_ptr const &rhs);
 
-    // -----------------------------------------------------------------------------------------------------------------
+    // ----------------------------------------------------------------------------------------------------------------
 
     bool operator != (std::nullptr_t, any_ptr const &rhs);
     bool operator != (std::nullptr_t, const_any_ptr const &rhs);
@@ -43,22 +39,26 @@ namespace m1
     bool operator != (const_any_ptr const &lhs, any_ptr const &rhs);
     bool operator != (const_any_ptr const &lhs, const_any_ptr const &rhs);
 
-    // -----------------------------------------------------------------------------------------------------------------
+    // ----------------------------------------------------------------------------------------------------------------
 
     template <typename T> T* reinterpret_pointer_cast(any_ptr const &p);
     template <typename T> T const* reinterpret_pointer_cast(const_any_ptr const &p);
 
-    // -----------------------------------------------------------------------------------------------------------------
+    // ----------------------------------------------------------------------------------------------------------------
 
     template <typename T> T* static_pointer_cast(any_ptr const &p);
     template <typename T> T const* static_pointer_cast(const_any_ptr const &p);
 
-    // -----------------------------------------------------------------------------------------------------------------
+    // ----------------------------------------------------------------------------------------------------------------
 
     template <typename T> T* dynamic_pointer_cast(any_ptr const &p);
     template <typename T> T const* dynamic_pointer_cast(const_any_ptr const &p);
 
-    // =================================================================================================================
+    // ================================================================================================================
+
+    typedef void (*any_ptr_type_id)();
+
+    // ================================================================================================================
 
     class any_ptr
     {
@@ -90,14 +90,14 @@ namespace m1
         template <typename T> friend T* dynamic_pointer_cast(any_ptr const &p);
 
     private:
-        any_ptr(void * const ptr, type_id_t const type_id);
+        any_ptr(void * ptr, any_ptr_type_id type_id);
 
         // members:
         void *m_Ptr = nullptr;
-        type_id_t m_TypeId = nullptr;
+        any_ptr_type_id m_TypeId = nullptr;
     };
 
-    // =================================================================================================================
+    // ================================================================================================================
 
     class const_any_ptr
     {
@@ -131,79 +131,80 @@ namespace m1
         template <typename T> friend T const* dynamic_pointer_cast(const_any_ptr const &p);
 
     private:
-        const_any_ptr(void const * const ptr, type_id_t const type_id);
+        const_any_ptr(void const *ptr, any_ptr_type_id type_id);
 
         // members:
         void const *m_Ptr = nullptr;
-        type_id_t m_TypeId = nullptr;
+        any_ptr_type_id m_TypeId = nullptr;
     };
 
-    // =================================================================================================================
+    // ================================================================================================================
 } // namespace m1
 
-// =====================================================================================================================
+// ====================================================================================================================
 // implementation
-// =====================================================================================================================
+// ====================================================================================================================
 
 namespace m1
 {
-    // =================================================================================================================
+    // ================================================================================================================
 
+    template <typename T, int Line>
     struct is_complete_type_impl
     {
         typedef char no_type[1];
         typedef char yes_type[2];
 
-        template <typename T, int Line> static no_type& test_impl(...);
-        template <typename T, int Line> static yes_type& test_impl(int (*)[sizeof(T)]);
+        static no_type& test_impl(...);
+        static yes_type& test_impl(T);
 
-        template <typename T, int Line> constexpr static bool test()
+        constexpr static bool test()
         {
-            return (sizeof(test_impl<T, Line>(nullptr)) == sizeof(yes_type));
+            return (sizeof(test_impl(std::declval<T>())) == sizeof(yes_type));
         }
     };
 
     template <typename T, int Line>
     struct is_complete_type
-        : std::integral_constant<bool, is_complete_type_impl::test<T, Line>()>
+        : std::integral_constant<bool, is_complete_type_impl<T, Line>::test()>
     {};
 
-    // =================================================================================================================
+    // ================================================================================================================
 
-    template <typename T> constexpr type_id_t get_type_id();
-    template <typename T> constexpr type_id_t get_type_id(std::true_type is_complete);
-    template <typename T> constexpr type_id_t get_type_id(std::false_type is_complete);
+    template <typename T> constexpr any_ptr_type_id get_any_ptr_type_id();
+    template <typename T> constexpr any_ptr_type_id get_any_ptr_type_id(std::true_type is_complete);
+    template <typename T> constexpr any_ptr_type_id get_any_ptr_type_id(std::false_type is_complete);
 
-    // -----------------------------------------------------------------------------------------------------------------
+    // ----------------------------------------------------------------------------------------------------------------
 
-    template <typename T> void throw_type_id_func();
+    template <typename T> void throw_any_ptr_type_id_func();
 
-    // -----------------------------------------------------------------------------------------------------------------
+    // ----------------------------------------------------------------------------------------------------------------
 
-    template <typename T> void null_type_id_func();
+    template <typename T> void null_any_ptr_type_id_func();
 
-    // =================================================================================================================
+    // ================================================================================================================
 } // namespace m1
 
-// =====================================================================================================================
+// ====================================================================================================================
 // m1::any_ptr
-// =====================================================================================================================
+// ====================================================================================================================
 
 template <typename T> m1::any_ptr::any_ptr(T * const rhs)
-    : any_ptr(rhs, get_type_id<T>())
+    : any_ptr(rhs, get_any_ptr_type_id<T>())
 {
 }
 
-// ---------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 
 template <typename T> m1::any_ptr& m1::any_ptr::operator = (T * const rhs)
 {
     m_Ptr = rhs;
-    m_TypeId = get_type_id<T>();
+    m_TypeId = get_any_ptr_type_id<T>();
     return *this;
 }
 
-// =====================================================================================================================
+// ====================================================================================================================
 
 template <typename T> T* m1::reinterpret_pointer_cast(any_ptr const &p)
 {
@@ -211,26 +212,26 @@ template <typename T> T* m1::reinterpret_pointer_cast(any_ptr const &p)
     return static_cast<T*>(p.m_Ptr);
 }
 
-// =====================================================================================================================
+// ====================================================================================================================
 
 template <typename T> T* m1::static_pointer_cast(any_ptr const &p)
 {
-    return (p.m_TypeId == get_type_id<T>())
+    return (p.m_TypeId == get_any_ptr_type_id<T>())
            ? static_cast<T*>(p.m_Ptr)
            : nullptr;
 }
 
-// =====================================================================================================================
+// ====================================================================================================================
 
 template <typename T> T* m1::dynamic_pointer_cast(any_ptr const &p)
 {
+    if(T * const result = static_pointer_cast<T>(p))
+    {
+        return result;
+    }
+
     try
     {
-        if(T * const result = static_pointer_cast<T>(p))
-        {
-            return result;
-        }
-
         if(p.m_TypeId) p.m_TypeId();
         return nullptr;
     }
@@ -244,25 +245,25 @@ template <typename T> T* m1::dynamic_pointer_cast(any_ptr const &p)
     }
 }
 
-// =====================================================================================================================
+// ====================================================================================================================
 // m1::const_any_ptr
-// =====================================================================================================================
+// ====================================================================================================================
 
 template <typename T> m1::const_any_ptr::const_any_ptr(T const * const rhs)
-    : const_any_ptr(rhs, get_type_id<T>())
+    : const_any_ptr(rhs, get_any_ptr_type_id<T>())
 {
 }
 
-// ---------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 
 template <typename T> m1::const_any_ptr& m1::const_any_ptr::operator = (T const * const rhs)
 {
     m_Ptr = rhs;
-    m_TypeId = get_type_id<T>();
+    m_TypeId = get_any_ptr_type_id<T>();
     return *this;
 }
 
-// =====================================================================================================================
+// ====================================================================================================================
 
 template <typename T> T const* m1::reinterpret_pointer_cast(const_any_ptr const &p)
 {
@@ -270,26 +271,26 @@ template <typename T> T const* m1::reinterpret_pointer_cast(const_any_ptr const 
     return static_cast<T const*>(p.m_Ptr);
 }
 
-// =====================================================================================================================
+// ====================================================================================================================
 
 template <typename T> T const* m1::static_pointer_cast(const_any_ptr const &p)
 {
-    return (p.m_TypeId == get_type_id<T>())
+    return (p.m_TypeId == get_any_ptr_type_id<T>())
            ? static_cast<T const*>(p.m_Ptr)
            : nullptr;
 }
 
-// =====================================================================================================================
+// ====================================================================================================================
 
 template <typename T> T const* m1::dynamic_pointer_cast(const_any_ptr const &p)
 {
+    if(T const * const result = static_pointer_cast<T>(p))
+    {
+        return result;
+    }
+
     try
     {
-        if(T const * const result = static_pointer_cast<T>(p))
-        {
-            return result;
-        }
-
         if(p.m_TypeId) p.m_TypeId();
         return nullptr;
     }
@@ -303,42 +304,42 @@ template <typename T> T const* m1::dynamic_pointer_cast(const_any_ptr const &p)
     }
 }
 
-// =====================================================================================================================
-// m1::type_id
-// =====================================================================================================================
+// ====================================================================================================================
+// m1::any_ptr_type_id
+// ====================================================================================================================
 
-template <typename T> void m1::throw_type_id_func()
+template <typename T> void m1::throw_any_ptr_type_id_func()
 {
     throw static_cast<T*>(nullptr);
 }
 
-// =====================================================================================================================
+// ====================================================================================================================
 
-template <typename T> void m1::null_type_id_func()
+template <typename T> void m1::null_any_ptr_type_id_func()
 {
 }
 
-// =====================================================================================================================
+// ====================================================================================================================
 
-template <typename T> constexpr m1::type_id_t m1::get_type_id()
+template <typename T> constexpr m1::any_ptr_type_id m1::get_any_ptr_type_id()
 {
-    return get_type_id<T>(is_complete_type<T, __LINE__>());
+    return get_any_ptr_type_id<T>(is_complete_type<T, __LINE__>());
 }
 
-// ---------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 
-template <typename T> constexpr m1::type_id_t m1::get_type_id(std::true_type)
+template <typename T> constexpr m1::any_ptr_type_id m1::get_any_ptr_type_id(std::true_type const /*is_complete*/)
 {
-    return &throw_type_id_func<T>;
+    return &throw_any_ptr_type_id_func<T>;
 }
 
-// ---------------------------------------------------------------------------------------------------------------------
+// --------------------------------------------------------------------------------------------------------------------
 
-template <typename T> constexpr m1::type_id_t m1::get_type_id(std::false_type)
+template <typename T> constexpr m1::any_ptr_type_id m1::get_any_ptr_type_id(std::false_type const /*is_complete*/)
 {
-    return &null_type_id_func<T>;
+    return &null_any_ptr_type_id_func<T>;
 }
 
-// =====================================================================================================================
+// ====================================================================================================================
 
 #endif // M1_ANY_PTR_HPP
